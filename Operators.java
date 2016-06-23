7a7e6b8a7d786a624328edab6edf27b8bf035d14
Devyash Sanghai <andiosdroid@gmail.com>
2016-06-23 17:38:18 +0530

    Intitialized the operators class

---
+++ Operators.java
@@ -0,0 +1,38 @@
+class Operators{
+	int arithmetic(int x, int y){
+		return 0;
+
+	}
+	int compound(){
+		return 0;
+
+	}
+	int incrementAndDecrement(){
+		return 0;
+
+	}
+	int relational(){
+		return 0;
+
+	}
+	int booleanOp(){
+		return 0;
+
+	}
+	int bitwise(){
+		return 0;
+
+	}
+	int conversion(){
+		return 0;
+
+	}
+	int commandline(){
+		return 0;
+
+	}
+	public static void main (String... args){
+		System.out.println("Main Method!");
+
+	}
+}
