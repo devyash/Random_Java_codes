e836c3c4bf5df0f12180d6d8b201264bc371a0cf
Devyash Sanghai <andiosdroid@gmail.com>
2016-06-25 19:29:55 +0530

    Op class

--- Operators.java
+++ Operators.java
@@ -1,5 +1,14 @@
 class Operators{
-	int arithmetic(int x, int y){
+	int arithmetic(double x, double y,char a){
+		System.out.println("x="+x+"  y="+y);
+		System.out.println();
+		System.out.println("_________________________________________");
+		System.out.println("x+y="+(x+y));
+		System.out.println("x%y="+(x%y));
+		System.out.println("x/="+(x/=2));
+		System.out.println("--------------------");
+		System.out.println("a+5= "+(a+5));
+		System.out.println("a+\'5\'=  "+(a+'5'));
 		return 0;
 
 	}
@@ -15,7 +24,10 @@ class Operators{
 		return 0;
 
 	}
-	int booleanOp(){
+	int booleanOp(boolean a){
+		a^=false;	
+		System.out.println("a= "+a);
+
 		return 0;
 
 	}
@@ -33,6 +45,9 @@ class Operators{
 	}
 	public static void main (String... args){
 		System.out.println("Main Method!");
-
+		System.out.println("_________________________________________");
+		Operators obj1= new Operators();
+		obj1.arithmetic(Float.parseFloat(args[0]), Float.parseFloat(args[1]),args[2].charAt(0));
+		obj1.booleanOp(Boolean.parseBoolean(args[3]));
 	}
 }
